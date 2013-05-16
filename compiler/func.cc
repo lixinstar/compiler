c7855d2fb2a39b259f55a2fac6b9597442618f6d
LiXin <lixin.star3@gmail.com>
2013-05-16 22:06:48 +0800

    实现了与保留字表的匹配

--- compiler/func.cc
+++ compiler/func.cc
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cctype>
 #include "compiler.h"
+
 using namespace std;
 
 #define MY_DEBUG
@@ -32,7 +33,7 @@ vpsi import_reserved( void )
 #ifdef MY_DEBUG_H
 	vpsi::iterator iter = vec.begin();
 	while (iter != vec.end()) {
-	  	cout << iter->first << " " << iter->second;
+	  	cout << iter->first << " " << iter->second << endl;
 		iter++;
 	}
 	cout << endl;
@@ -129,7 +130,7 @@ loop:	while (iter != source.end()) {
 		iter++;
 	}
 
-#ifdef 	MY_DEBUG
+#ifdef 	MY_DEBUG_H
 	vector<string>::const_iterator jter = new_source.begin();
 	while (jter != new_source.end()) {
 		cout << *jter << endl;
@@ -138,4 +139,104 @@ loop:	while (iter != source.end()) {
 #endif
 
 	return new_source;
+}
+
+// 将分析好的源文件与保留字表对比
+vpsi compare(vpsi reserved, vector<string> 	source)
+{
+	vpsi 	result;
+//	vpsi::const_iterator 	iter = reserved.begin();
+	vpsi::const_iterator 	kter = reserved.begin();
+	vpsi::const_iterator 	pointer;
+	vector<string>::iterator 	ster = source.begin();
+
+	// 遍历reserved表，找到符号的开始地址，用pointer保留首地址
+	while (kter != reserved.end()) {
+		if (!isalpha(*((*kter).first.begin())))
+		  break;
+		++kter;
+	}
+	pointer = kter;
+	psi 	ptemp;
+
+	// 开始进行匹配
+	while (ster != source.end()) {
+		kter = reserved.begin();
+		// 如果ster是字母，则在此处理，进行关键字匹配
+		if (isalpha(*((*ster).begin()))) {
+			while (kter != pointer) {
+				if ((*kter).first == *ster) {
+					result.push_back(*kter);
+					break;
+				}
+				kter++;
+			}
+			// 如果关键字中没有,则做此处理
+			if (kter == pointer) {
+				ptemp = make_pair(*ster, (*(kter - 1)).second);
+				result.push_back(ptemp);
+			}
+		}
+		// 如果是非字母,则在此处理
+		else {
+			// 如果是>=号
+			if (*ster == ">") {
+				if (*(ster + 1) == "=") {
+					while (kter != reserved.end()) {
+						if ((*kter).first == ">=")
+							break;
+						kter++;
+					}
+				}	
+			} 
+			// 如果是<=号
+			else if (*ster == "<") {
+				if (*(ster + 1) == "=") {
+					while (kter != reserved.end()) {
+						if ((*kter).first == "<=") 
+							break;
+						kter++;
+					}
+				}
+			} 
+			// 如果是\n
+			else if (*ster == "\\") {
+				if (*(ster + 1) == "n") {
+					while (kter != reserved.end()) {
+						if ((*kter).first == "\\n") {
+							break;
+						}
+						kter++;
+					}
+				}
+			} 
+			// 如果是其他的关键字
+			else {
+				while (kter != reserved.end()) {
+					if ((*kter).first == *ster)
+						break;
+					kter++;
+				}
+			}
+			if (kter != reserved.end()) {
+				result.push_back(*kter);
+			} 
+			// 如果关键字表中没有,则做此处理	
+			else {
+				ptemp = make_pair(*ster, (*(kter - 1)).second);
+				result.push_back(ptemp);
+			}
+		}
+		++ster;
+	}
+
+#ifdef 	MY_DEBUG
+	vpsi::const_iterator temp = result.begin();
+	while (temp != result.end()) {
+		cout << (*temp).first << "\t" << (*temp).second << endl;
+		++temp;
+	}
+#endif
+
+	return result;
 }
